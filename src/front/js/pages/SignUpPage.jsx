ff1c21283e7f3cc59250673cde0006d76c08b9a2
Felop2299 <pipe-220899@hotmail.com>
2025-02-22 05:29:32 +0000

    cambios a los componentes de registro, tambien agrege botones al home para cuando se abra poder navegar al registro y al login

---
+++ src/front/js/pages/SignUpPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SignUp from "../component/SignUp.jsx";
+
+const SignUpPage = () => {
+  return (
+    <div className="login-container">
+      <h2>Página de Sign Up</h2>
+      <SignUp/>
+    </div>
+  );
+};
+
+export default SignUpPage;
