9e2157cf9f096e65f786f0e15a9bb64617875bfd
Maria Daniela Martin <m.danielamartin@proton.me>
2025-02-28 00:07:39 +0000

    fixed errors on restaurant/builer/menu pages, added more functionalities to dashboard

--- migrations/versions/e0de18d0c46e_.py
+++ migrations/versions/e0de18d0c46e_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 41aadf70d33e
+Revision ID: e0de18d0c46e
 Revises: 
-Create Date: 2025-02-25 04:03:55.520514
+Create Date: 2025-02-26 00:23:57.770172
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '41aadf70d33e'
+revision = 'e0de18d0c46e'
 down_revision = None
 branch_labels = None
 depends_on = None
---
+++ src/front/js/component/CreateMenuModal.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useContext } from "react";
+import { Context } from "../store/appContext";
+import Button from 'react-bootstrap/Button';
+import { Form, Dropdown, DropdownButton } from "react-bootstrap";
+import Modal from 'react-bootstrap/Modal';
+import { useNavigate } from "react-router-dom";
+
+function CreateMenuModal({ restaurantID }) {
+    const { store, actions } = useContext(Context);
+    const [show, setShow] = useState(false);
+    const [newMenu, setNewMenu] = useState({ 'name': '', 'restaurantID': restaurantID, 'currency': 'Selecciona una moneda' });
+    const navigate = useNavigate()
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
+    const handleCreate = async () => {
+        if (newMenu.currency.length > 3 || !newMenu.name) {
+            alert('Debes escoger un nombre y una moneda para crear un menu')
+        }
+        const response = await actions.createMenu(newMenu)
+        if (response) {
+            navigate(`/menu-builder/${response.id}`)
+            setShow(false)
+        }
+    }
+
+    const handleSelect = (eventKey) => {
+        setNewMenu({ ...newMenu, currency: eventKey })
+    }
+
+    return (
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Crear Nuevo Menú
+            </Button>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Datos del Menú</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Control
+                            type="text"
+                            placeholder="Nombre el Menú"
+                            value={newMenu.name}
+                            onChange={(e) => setNewMenu({ ...newMenu, name: e.target.value })}
+                        />
+                        <DropdownButton className="mt-3" id="dropdown-basic-button" title={newMenu.currency} onSelect={handleSelect}>
+                            <Dropdown.Item eventKey="COP">Pesos Colombianos</Dropdown.Item>
+                            <Dropdown.Item eventKey="USD">Dólares Americanos</Dropdown.Item>
+                            <Dropdown.Item eventKey="CAD">Dólares Canadienses</Dropdown.Item>
+                            <Dropdown.Item eventKey="EUR">Euros</Dropdown.Item>
+                        </DropdownButton>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Cerrar
+                    </Button>
+                    <Button variant="primary" onClick={handleCreate}>Crear Menú</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default CreateMenuModal;
