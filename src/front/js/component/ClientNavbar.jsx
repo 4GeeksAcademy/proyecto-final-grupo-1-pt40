97f78f9f45f4bac031db4323b5d33fdb4162c16d
Maria Daniela Martin <m.danielamartin@proton.me>
2025-03-14 16:44:47 +0000

    added logout button, new menuView, fixed in case no menus

--- src/front/js/component/ClientNavbar.jsx
+++ src/front/js/component/ClientNavbar.jsx
@@ -1,46 +1,50 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import LogoutButton from "./LogoutButton.jsx";
 
 const ClientNavbar = ({ activeMenu, setActiveMenu }) => {
-    return (
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand>El punto</Navbar.Brand>
-          <Nav className="ml-auto">
-            <Nav.Link
-              onClick={() => setActiveMenu("restaurants")}
-              active={activeMenu === "restaurants"}
-            >
-              Restaurantes
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => setActiveMenu("explore")}
-              active={activeMenu === "explore"}
-            >
-              Explora
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => setActiveMenu("perfil")}
-              active={activeMenu === "perfil"}
-            >
-              Perfil
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => setActiveMenu("favoritos")}
-              active={activeMenu === "favoritos"}
-            >
-              Favoritos
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => setActiveMenu("ayuda")}
-              active={activeMenu === "ayuda"}
-            >
-              Ayuda
-            </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    );
-  };
-  
-  export default ClientNavbar;
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand>El punto</Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link
+            onClick={() => setActiveMenu("restaurants")}
+            active={activeMenu === "restaurants"}
+          >
+            Restaurantes
+          </Nav.Link>
+          <Nav.Link
+            onClick={() => setActiveMenu("explore")}
+            active={activeMenu === "explore"}
+          >
+            Explora
+          </Nav.Link>
+          <Nav.Link
+            onClick={() => setActiveMenu("perfil")}
+            active={activeMenu === "perfil"}
+          >
+            Perfil
+          </Nav.Link>
+          <Nav.Link
+            onClick={() => setActiveMenu("favoritos")}
+            active={activeMenu === "favoritos"}
+          >
+            Favoritos
+          </Nav.Link>
+          <Nav.Link
+            onClick={() => setActiveMenu("ayuda")}
+            active={activeMenu === "ayuda"}
+          >
+            Ayuda
+          </Nav.Link>
+          <Nav.Link>
+            <LogoutButton/>
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default ClientNavbar;
--- src/front/js/pages/MenuView.jsx
+++ src/front/js/pages/MenuView.jsx
@@ -1,24 +1,42 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Tabs, Tab, Card, Button } from "react-bootstrap";
+import { Tabs, Tab, Card, Button, Container, Row, Col, Badge, Nav } from "react-bootstrap";
 import Spinner from 'react-bootstrap/Spinner';
 import { useParams } from 'react-router-dom';
 import { Context } from "../store/appContext";
-import FavoriteButton from "../component/FavoriteButton.jsx"
+import GoogleMapsModal from "../component/GoogleMapsModal.jsx";
 
 
 const MenuView = () => {
-    const { menuID } = useParams();
+    const { menu_id } = useParams();
     const { store, actions } = useContext(Context);
     const [menu, setMenu] = useState(null)
     const [dishes, setDishes] = useState(null)
-    const [like, setLike] = useState(false)
+    const [restaurant, setRestaurant] = useState(null)
+
 
     const onLoad = async () => {
-        const response = await actions.menuViewLoad(menuID)
+        console.log('here')
+        const response = await actions.menuViewLoad(menu_id)
         if (response) {
             setMenu(store.menu.menu)
             console.log(store.menu.menu)
             setDishes(store.menu.dishes)
+            setRestaurant(store.menu.restaurant)
+        }
+    }
+
+    const convertToAmPm = (time) => {
+        if (!time) return "";
+        const [hour, minute] = time.split(":").map(Number);
+        const ampm = hour >= 12 ? "PM" : "AM";
+        const formattedHour = hour % 12 === 0 ? 12 : hour % 12;
+        return `${formattedHour}:${minute.toString().padStart(2, "0")} ${ampm}`
+    }
+
+    const handleScroll = (catID) => {
+        const scrollTo = document.getElementById(catID)
+        if (scrollTo) {
+            scrollTo.scrollIntoView({ "behavior": "smooth", "block": "start" })
         }
     }
 
@@ -26,49 +44,104 @@ const MenuView = () => {
         onLoad()
     }, []);
 
+
+    if (!menu || !dishes || !restaurant) {
+        return (
+            <Container fluid>
+                <Spinner animation="border" variant="danger" />
+            </Container>
+        )
+    }
+
     return (
-        <div>
-            {menu && dishes ? (
-                <>
-                    <h1 className="d-flex justify-content-center">{menu.name}</h1>
-                    {menu.categories.length > 0 ? (
-                        <Tabs defaultActiveKey={menu.categories[0]} id="fill-tab-example" className="mb-3" fill>
-                            {menu.categories.map((cat) => (
-                                <Tab eventKey={cat} title={cat} key={cat}>
-                                    {
-                                        Array.isArray(dishes[cat]) ? (
-                                            dishes[cat].map((dish, index) => (
-                                                <Card key={index} style={{ width: "200px", height: "340px" }} className="m-2 d-flex">
-                                                    <Card.Img variant="top" src={dish.image}
-                                                        style={{ width: '100%', height: '200px' }} />
-                                                    <Card.Body>
-                                                        <Card.Title>{dish.name}</Card.Title>
-                                                        <Card.Text>{dish.description}</Card.Text>
-                                                        <Card.Text><strong>Precio:</strong> {`${dish.price} ${menu.currency}`}</Card.Text>
-                                                        
-                                                        {store.client?.client_id &&<FavoriteButton dish_id={dish.dish_id} />}
-                                                    </Card.Body>
+        <Container fluid className="m-1">
+            <Row className="w-25">
+                <div className={menu.is_active ? ('bg-success') : ('bg-danger')}>
+                    <span className="fs-5"><strong>Estatus:</strong> {`${menu.is_active ? ('PÚBLICO') : ('PRIVADO')}`}</span>
+                </div>
+            </Row>
+            <Row className="w-100 h-100">
+                <Col xs md lg="5" className="justify-content-start h-100">
+                    <Card className="w-100">
+                        {dishes && restaurant && menu ?
+                            (<>
+                                <Card.Img variant="top" src={restaurant.image} />
+                                <Card.Body>
+                                    <Card.Title>{restaurant.name}</Card.Title>
+                                    <Card.Subtitle> <Badge bg="light" className="p-3 text-dark">{restaurant.cuisine_type}</Badge> <GoogleMapsModal addressLink={restaurant.social_networks} /></Card.Subtitle>
+                                    <Card.Text>{`${restaurant.exact_address}  ${restaurant.city}, ${restaurant.department}`} </Card.Text>
+
+                                    {restaurant.schedule.map((day, index) => (
+                                        <div className="p-0 m-0" key={index}>{`${day.day}: ${day.isClosed ? 'Cerrado' : `${convertToAmPm(day.open)} - ${convertToAmPm(day.close)}`}`}</div>
+                                    ))}
+
+
+                                </Card.Body></>)
+
+                            : (<Card.Body>
+                                <Card.Title>Cargando ....</Card.Title>
+                            </Card.Body>)
+
+                        }
+                    </Card>
+
+                </Col>
+                <Col xs md lg="7">
+                    <Row className="sticky-top py-2 w-100">
+                        {menu && dishes && menu.categories && menu.categories.length > 0 ? (
+                            <Nav variant="pills" className="mb-3 bg-white p-3 rounded">
+                                {menu.categories.map((cat, index) => (
+                                    <Nav.Item key={index} className="mx-2">
+                                        <Button onClick={() => handleScroll(cat)}>{cat}</Button>
+                                    </Nav.Item>
+                                ))}
+                            </Nav>
+                        ) : (
+                            <div>Sin categorias</div>
+                        )}
+
+                    </Row>
+
+                    <Row className="w-100">
+                        {dishes && menu && menu.categories && menu.categories.length > 0 ? (
+                            <Col style={{ height: '100vh', overflowY: 'auto' }}>
+                                {menu.categories.map((cat, index) => (
+                                    <React.Fragment key={index}>
+                                        <div id={cat}>
+                                            <h3 className="my-2">{cat}</h3>
+                                        </div>
+                                        {Array.isArray(dishes[cat]) && dishes[cat].length > 0 ? (
+                                            dishes[cat].map((dish, dishIndex) => (
+                                                <Card className="my-2" key={dishIndex}>
+                                                    <Row className="w-100">
+                                                        <Col md='4'>
+                                                            <Card.Img variant="top" src={dish.image} alt='Sin imagen' style={{ 'width': '200px', 'height': '150px' }} />
+                                                        </Col>
+                                                        <Col md='7'>
+                                                            <Card.Body>
+                                                                <Card.Title>{dish.name}</Card.Title>
+                                                                <Card.Text>{dish.description}</Card.Text>
+                                                                <Card.Text><strong>Precio:</strong> {`${dish.price} ${menu.currency}`}</Card.Text>
+                                                            </Card.Body>
+                                                        </Col>
+                                                    </Row>
                                                 </Card>
                                             ))
-                                        ) : ( 
-                                            'No hay platillos en esta categoria'
-                                        )
-                                    }
-                                </Tab>
-                            ))}
-                        </Tabs>
-                    ) : (
-                        <Tabs defaultActiveKey="profile" id="fill-tab-example" className="mb-3" fill>
-                            <Tab eventKey='platillos' title='Platillos' key='sincategorias'>
-                                No hay platillos en este menu
-                            </Tab>
-                        </Tabs>
-                    )}
-                </>
-            ) : (
-                <Spinner animation="border" variant="danger" />
-            )}
-        </div>
+                                        ) : (
+                                            <div>No hay platillos en esta categoria</div>
+                                        )}
+                                    </React.Fragment>
+                                ))}
+                            </Col>
+                        ) : (
+                            <div>Menu sin elementos</div>
+                        )}
+
+                    </Row>
+                </Col>
+
+            </Row>
+        </Container>
     );
 };
 
