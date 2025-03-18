545731d38640b36b0628fbec57b9c6bbbdc306d7
Felop2299 <pipe-220899@hotmail.com>
2025-03-18 03:59:55 +0000

    jaajja correccion

--- src/front/js/pages/MenuPublicView.jsx
+++ src/front/js/pages/MenuPublicView.jsx
@@ -75,34 +75,42 @@ const MenuPublicView = () => {
             <ClientNavbar />
             <Container fluid className="m-1">
                 <Row>
-                    {menuList && restaurant_username && menu_id ? <MenuNavigation username={restaurant_username} menus={menuList} selected={menu_id} /> : <div>Cargando...</div>}
+                    {menuList && restaurant_username && menu_id ? 
+                        <MenuNavigation username={restaurant_username} menus={menuList} selected={menu_id} /> : 
+                        <div>Cargando...</div>
+                    }
                 </Row>
                 <Row className="w-100 h-100">
                     <Col xs md lg="5" className="justify-content-start h-100">
                         <Card className="w-100">
-                            {dishes && restaurant && menu ?
-                                (<>
+                            {dishes && restaurant && menu ? (
+                                <>
                                     <Card.Img variant="top" src={restaurant.image} />
                                     <Card.Body>
                                         <Card.Title>{restaurant.name}</Card.Title>
-                                        <Card.Subtitle> <Badge bg="light" className="p-3 text-dark">{restaurant.cuisine_type}</Badge> <GoogleMapsModal addressLink={restaurant.social_networks} /> {isLogged && <FavoriteButton dish_id={null} restaurant_id={restaurant.restaurant_id} />}</Card.Subtitle>
-                                        <Card.Text>{`${restaurant.exact_address}  ${restaurant.city}, ${restaurant.department}`} </Card.Text>
-
+                                        <Card.Subtitle>
+                                            <Badge bg="light" className="p-3 text-dark">{restaurant.cuisine_type}</Badge> 
+                                            <GoogleMapsModal addressLink={restaurant.social_networks} /> 
+                                            {isLogged && <FavoriteButton dish_id={null} restaurant_id={restaurant.restaurant_id} />}
+                                        </Card.Subtitle>
+                                        <Card.Text>{`${restaurant.exact_address}  ${restaurant.city}, ${restaurant.department}`}</Card.Text>
+    
                                         {restaurant.schedule.map((day, index) => (
-                                            <div className="p-0 m-0" key={index}>{`${day.day}: ${day.isClosed ? 'Cerrado' : `${convertToAmPm(day.open)} - ${convertToAmPm(day.close)}`}`}</div>
+                                            <div className="p-0 m-0" key={index}>
+                                                {`${day.day}: ${day.isClosed ? 'Cerrado' : `${convertToAmPm(day.open)} - ${convertToAmPm(day.close)}`}`}
+                                            </div>
                                         ))}
-
+    
                                         <Button className="bg-secondary mt-3 self-align-center">Reportar Restaurante</Button>
-
-                                    </Card.Body></>)
-
-                                : (<Card.Body>
+                                        <ClientReportModal restaurant_id={restaurant.restaurant_id} />
+                                    </Card.Body>
+                                </>
+                            ) : (
+                                <Card.Body>
                                     <Card.Title>Cargando ....</Card.Title>
-                                </Card.Body>)
-
-                            }
+                                </Card.Body>
+                            )}
                         </Card>
-
                     </Col>
                     <Col xs md lg="7">
                         <Row className="sticky-top py-2 w-100">
@@ -117,9 +125,8 @@ const MenuPublicView = () => {
                             ) : (
                                 <div>Sin categorias</div>
                             )}
-
                         </Row>
-
+    
                         <Row className="w-100">
                             {dishes && menu && menu.categories && menu.categories.length > 0 ? (
                                 <Col style={{ height: '100vh', overflowY: 'auto' }}>
@@ -157,87 +164,11 @@ const MenuPublicView = () => {
                             ) : (
                                 <div>Menu sin elementos</div>
                             )}
-
                         </Row>
                     </Col>
-
-                                    
-                                        <ClientReportModal restaurant_id={restaurant.restaurant_id} />
-                                    
-
-                                </Card.Body></>)
-
-                            : (<Card.Body>
-                                <Card.Title>Cargando ....</Card.Title>
-                            </Card.Body>)
-
-                        }
-                    </Card>
-
-                </Col>
-                <Col xs md lg="7">
-                    <Row className="sticky-top py-2 w-100">
-                        {menu && dishes && menu.categories && menu.categories.length > 0 ? (
-                            <Nav variant="pills" className="mb-3 bg-white p-3 rounded">
-                                {menu.categories.map((cat, index) => (
-                                    <Nav.Item key={index} className="mx-2">
-                                        <Button onClick={() => handleScroll(cat)}>{cat}</Button>
-                                    </Nav.Item>
-                                ))}
-                            </Nav>
-                        ) : (
-                            <div>Sin categorias</div>
-                        )}
-
-                    </Row>
-
-                    <Row className="w-100">
-                        {dishes && menu && menu.categories && menu.categories.length > 0 ? (
-                            <Col style={{ height: '100vh', overflowY: 'auto' }}>
-                                {menu.categories.map((cat, index) => (
-                                    <React.Fragment key={index}>
-                                        <div id={cat}>
-                                            <h3 className="my-2">{cat}</h3>
-                                        </div>
-                                        {Array.isArray(dishes[cat]) && dishes[cat].length > 0 ? (
-                                            dishes[cat].map((dish, dishIndex) => (
-                                                <Card className="my-2" key={dishIndex}>
-                                                    <Row className="w-100">
-                                                        <Col md='4'>
-                                                            <Card.Img variant="top" src={dish.image} alt='Sin imagen' style={{ 'width': '200px', 'height': '150px' }} />
-                                                        </Col>
-                                                        <Col md='7'>
-                                                            <Card.Body>
-                                                                <Card.Title>{dish.name}</Card.Title>
-                                                                <Card.Text>{dish.description}</Card.Text>
-                                                                <Card.Text><strong>Precio:</strong> {`${dish.price} ${menu.currency}`}</Card.Text>
-                                                            </Card.Body>
-                                                        </Col>
-                                                        <Col md='1' className="justify-content-center align-middle mt-2">
-                                                            {isLogged && <FavoriteButton dish_id={dish.dish_id} restaurant_id={null} />}
-                                                        </Col>
-                                                    </Row>
-                                                </Card>
-                                            ))
-                                        ) : (
-                                            <div>No hay platillos en esta categoria</div>
-                                        )}
-                                    </React.Fragment>
-                                ))}
-                            </Col>
-                        ) : (
-                            <div>Menu sin elementos</div>
-                        )}
-
-                    </Row>
-                </Col>
-
-            </Row>
-        </Container>
-
-            
+                </Row>
+            </Container>
         </div>
-
     );
 };
 
